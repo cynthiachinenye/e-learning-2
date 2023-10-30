402d139e95b11f792028330a78a4e80b458ea528
cynthiachinenye <cynthiachiagu@gmail.com>
2023-10-30 13:00:08 +0100

    first-commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import '../Styles/nav.css'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="nav">
+    <div className='container'>
+    <div className='logo'>
+    <a href='#' className='img' ><img src='./Assets/logo.png' alt="logo"/></a>
+    </div>
+    <div className='list'>
+    <ul>
+      <li><a href='#' className='li act'>Home</a></li>
+      <li><a href='#' className='li'>Profile</a></li>
+      <li><a href='#' className='li'>Courses</a></li>
+      <li><a href='#' className='li'>Contact</a></li>
+    </ul>
+    </div>
+    <button>Sgin In</button>
+    
+    </div>
       
     </div>
   )
