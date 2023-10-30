867c3d758ac63d2d9fff593ed5b8bdccaf17f358
cynthiachinenye <cynthiachiagu@gmail.com>
2023-10-30 17:20:26 +0100

    header

---
+++ src/Components/Courses.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Courses = () => {
+  return (
+    <div className='main-container'>
+    <div className='container'>
+    <div className='img-box'>
+    <img src="./Assets/Group 114.png"/>
+    </div>
+    <div className='content'>
+    <h1>HOW OUR ONLINE PUBLIC SCHOOL WORKS</h1>
+    <div className='content-order'>
+    
+    
+    </div>
+    
+    </div>
+    
+    </div>
+      
+    </div>
+  )
+}
+
+export default Courses
