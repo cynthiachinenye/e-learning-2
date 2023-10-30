0931be1f01e27ec91b887e80c8829d55d95d8647
cynthiachinenye <cynthiachiagu@gmail.com>
2023-10-30 14:47:22 +0100

    header

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -16,6 +16,20 @@ const Profile = () => {
      <img src="./Assets/Image and card.png"/>
      </div>
     </div>
+    <div className='container'>
+     <div className='text'>
+     <h1>400+</h1>
+     <p>Universities worldwide that partner with us</p>
+     </div>
+     <div className='img-container'>
+     <img src='./Assets/image 112.png' className='img1' alt='images'/>
+     <img src='./Assets/image 113.png' className='img2' alt='images'/>
+     <img src='./Assets/image 114.png' className='img3' alt='images'/>
+     <img src='./Assets/image 115.png' className='img4' alt='images'/>
+     
+     </div>
+    
+    </div>
       
     </div>
   )
