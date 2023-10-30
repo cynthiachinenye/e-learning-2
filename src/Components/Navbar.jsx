953514c315781a4e6ca59fade34bcdc202e9415f
cynthiachinenye <cynthiachiagu@gmail.com>
2023-10-30 14:28:03 +0100

    header

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,10 +10,10 @@ const Navbar = () => {
     </div>
     <div className='list'>
     <ul>
-      <li><a href='#' className='li act'>Home</a></li>
-      <li><a href='#' className='li'>Profile</a></li>
-      <li><a href='#' className='li'>Courses</a></li>
-      <li><a href='#' className='li'>Contact</a></li>
+      <li><a href='#home' className='li act'>Home</a></li>
+      <li><a href='#profile' className='li'>Profile</a></li>
+      <li><a href='#courses' className='li'>Courses</a></li>
+      <li><a href='#contact' className='li'>Contact</a></li>
     </ul>
     </div>
     <button>Sgin In</button>
--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import '../Styles/profile.css'
 const Profile = () => {
   return (
-    <div>
+    <div className='main-container'>
+    <div className='container'>
+    
+     <div className='content'>
+     <h1>Discover best classes for the best learning</h1>
+     <p className='p'>We designed Connections Academy to give students all across the nation a tuition-free online public school that lets them learn.</p>
+     <button>Get Started</button>
+     <p>Not sure when to start? <span>Learn more</span></p>
+     
+     </div>
+     <div className='img-box'>
+     <img src="./Assets/Image and card.png"/>
+     </div>
+    </div>
       
     </div>
   )
