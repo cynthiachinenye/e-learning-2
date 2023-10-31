6e44c24676e5325b23f39ad382fd077bb6a65c90
cynthiachinenye <cynthiachiagu@gmail.com>
2023-10-31 15:59:52 +0100

    header

--- src/Components/Courses.jsx
+++ src/Components/Courses.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { LuMonitorPlay, LuBookOpenCheck } from 'react-icons/lu'
 import { BsJournalBookmark } from 'react-icons/bs'
-import {TbSocial} from 'react-icons/tb'
+import { TbSocial } from 'react-icons/tb'
+import '../Styles/courses.css'
 
 const Courses = () => {
   return (
@@ -11,36 +12,52 @@ const Courses = () => {
           <img src="./Assets/Group 114.png" />
         </div>
         <div className='content'>
-          <h1>HOW OUR ONLINE PUBLIC SCHOOL WORKS</h1>
-          <div className='content-order'>
-            <LuMonitorPlay />
-            <h5>VIRTUAL LEARNING</h5>
-            <p>The only costs are for standard school supplies and voluntary field trips.</p>
+          <h2>How Our Online Public School Works</h2>
+          <div className='icons'>
+
+            <div className='content-order'>
+            <div  className='div-icon'>
+            <LuMonitorPlay  className="icon"/>
+            </div>
+           
+              <h5>Virtual Learning</h5>
+              <p>The only costs are for standard school supplies and voluntary field trips.</p>
+
+            </div>
+
+            <div className='content-order'>
+            <div className='div-icon'>
+            <BsJournalBookmark className="icon" />
+            </div>
+              
+              <h5>Meaningful Education</h5>
+              <p>Many of our Connections Academy schools have additional accreditations.</p>
+
+            </div>
+
+            <div className='content-order'>
+            <div className='div-icon'>
+            <LuBookOpenCheck  className="icon"/>
+            </div>
+           
+              <h5>Online curriculum</h5>
+              <p>Our curriculum prepares students to go further in life by giving them support.</p>
+
+            </div>
+
+            <div className='content-order'>
+            <div className='div-icon'>
+            <TbSocial className="icon"/>
+            </div>
+              
+              <h5>Social Interaction</h5>
+              <p>Students at Connections Academy collaborate on projects together.</p>
+
+            </div>
+
+
 
           </div>
-
-          <div className='content-order'>
-            <BsJournalBookmark />
-            <h5>Meaningful Education</h5>
-            <p>Many of our Connections Academy schools have additional accreditations.</p>
-
-          </div>
-
-          <div className='content-order'>
-            <LuBookOpenCheck/>
-            <h5>ONLINE CURRICULUM</h5>
-            <p>Our curriculum prepares students to go further in life by giving them support.</p>
-
-          </div>
-
-          <div className='content-order'>
-            <TbSocial/>
-            <h5>SOCIAL INTERACTION</h5>
-            <p>Students at Connections Academy collaborate on projects together.</p>
-
-          </div>
-
-
         </div>
 
       </div>
