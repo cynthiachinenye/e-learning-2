226a2296e44747e2aaa14b93627f4d8f332dc32d
cynthiachinenye <cynthiachiagu@gmail.com>
2023-11-04 21:36:39 +0100

    new

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <div className='container'>
+      <div className='logo'>
+      <img src='./Assets/Logo.png' alt='image'/>
+      <p>Our motto to fulfill customer demand by making them satisfied with growing their business.</p>
+      
+      </div>
+      <div className='about'>
+      <h1>About</h1>
+      <ul>
+      <li>Features</li>
+      </ul>
+      
+      </div>
+      
+      </div>
+      
+    </div>
+  )
+}
+
+export default Footer
